49a1645a54d4c8c268e7b871cfa9542b006f7bf9
Adhiraj Singh <adhirajsingh1001@gmail.com>
2022-06-01 20:20:06 +0530

    docs: describe repos index

--- src/repos/index.ts
+++ src/repos/index.ts
@@ -3,6 +3,8 @@ import graphNodeDeployment from "./graph-node-deployment";
 import serviceValidator from "./service-validator";
 import subgraph from "./subgraph";
 
+// list out all repos that require CI
+// these would be repos that derive data from this single source of truth repo
 export default [
 	graphNodeDeployment,
 	serviceValidator,
