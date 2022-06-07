28cc2f37eeb86f16a54153502fbf7eb8c7100f69
Adhiraj Singh <adhirajsingh1001@gmail.com>
2022-06-07 14:22:57 +0530

    feat: CI for grants-contracts-upgradeable

--- src/repos/index.ts
+++ src/repos/index.ts
@@ -1,3 +1,4 @@
+import grantsContractsUpgradeable from "./grants-contracts-upgradeable";
 import grantsFrontend from "./grants-frontend";
 import graphNodeDeployment from "./graph-node-deployment";
 import serviceValidator from "./service-validator";
@@ -7,6 +8,7 @@ import subgraph from "./subgraph";
 // these would be repos that derive data from this single source of truth repo
 export default [
 	graphNodeDeployment,
+	grantsContractsUpgradeable,
 	serviceValidator,
 	subgraph,
 	grantsFrontend,
