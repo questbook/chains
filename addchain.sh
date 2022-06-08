217d836debc6acd5872265de4c3d7384223e1967
Sweta Shaw <swt.shw94@gmail.com>
2022-06-08 16:48:15 +0530

    chore: added shell script to run tasks

---
+++ addchain.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+while getopts "n:" arg; do
+  case $arg in
+    n) NETWORK=$OPTARG;;
+  esac
+done
+
+echo "Generating chains in grant-contracts-upgradable repo"
+ts-node src --repo grants-contracts-upgradeable
+
+cd contracts/evm 
+
+echo "installing packages.."
+yarn
+
+echo "generating types.."
+yarn typechain
+
+echo "deploying contracts"
+NETWORK=$NETWORK npm run deploy
+cd ../.. 
+
+echo "adding contract addresses and block numbers to your chain's yaml file"
+ts-node src/utils/gen-chain-yaml --chain-name $NETWORK
