4665e0d8efae1374a0363c73f1e5bda1a7fbbcfa
Richik Chanda <richikchanda1999@gmail.com>
2022-10-28 19:54:41 +0530

    chore: added CI for biconomy-server

---
+++ src/repos/biconomy-server.ts
@@ -0,0 +1,60 @@
+import { writeFile } from "fs/promises";
+import { join } from "path";
+import { CIRepo } from "../types";
+const CHAINS_DATA_FILE = './src/contracts/chainInfo.json'
+
+const biconomyServer: CIRepo = {
+	repoName: 'biconomy-server',
+	doCI: async(repoPath, chains) => {
+		// generate the chains.json file
+		const data = Object.values(chains).reduce(
+			(dict, chain) => {
+                const { qbContracts } = chain
+				if(typeof qbContracts !== 'object') {
+					throw new Error('QB contracts must be deployed before pushing to frontend')
+				}
+				dict[chain.chainId] = {
+					id: chain.chainId,
+					name: chain.userFacingName,
+					isTestNetwork: chain.isTestNetwork,
+                    icon: chain.icon,
+                    wallets: chain.supportedWallets,
+					explorer: chain.explorer,
+					supportedCurrencies: chain.supportedCurrencies.reduce(
+						(dict, currency) => {
+							dict[currency.address] = {
+                                label: currency.label,
+                                decimal: currency.decimals,
+                                address: currency.address,
+                            }
+							return dict
+						}, { } as { [_: string]: any }
+					),
+                    qbContracts: Object.keys(qbContracts).reduce(
+						(dict, contract) => {
+							dict[contract] = qbContracts[contract as keyof typeof qbContracts].address
+							return dict
+						}, { } as { [_: string]: string }
+					),
+					subgraphClientUrl: getSubgraphUrl(chain.chainName),
+					rpcUrls: chain.rpcUrls.map(
+						// replace any templates with empty strings
+						url => url.replace(/{{[a-z_-]+}}/gi, '')
+					),
+					nativeCurrency: chain.nativeCurrency ?? {
+						name: 'Unsupported Currency',
+						symbol: 'UNSUP',
+						decimals: 18,
+					}
+				}
+				return dict
+			}, { } as { [_: string]: any }
+		)
+
+		await writeFile(join(repoPath, CHAINS_DATA_FILE), JSON.stringify(data, null, '\t'))
+	}
+}
+
+const getSubgraphUrl = (chainName: string) => `https://the-graph.questbook.app/subgraphs/name/qb-subgraph-${chainName}`
+
+export default biconomyServer
